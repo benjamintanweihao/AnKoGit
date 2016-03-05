4d2fbb07f9391c7473703cde177446f2446aa743
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-05 09:15:34 +0800

    Using JUnit rule to search for right JSON response file.

--- app/src/test/java/io/benjamintan/ankogit/data/api/GitHubServiceTest.kt
+++ app/src/test/java/io/benjamintan/ankogit/data/api/GitHubServiceTest.kt
@@ -4,27 +4,33 @@ import io.benjamintan.ankogit.APIServiceTestHelper
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
 
 import org.mockito.Matchers.*
 
 class GitHubServiceTest {
 
     val server = MockWebServer()
+    var methodName: String = ""
+
+    @Rule @JvmField
+    var name = TestName()
 
     @Before
     fun setup() {
         server.start()
+        methodName = name.methodName.split("test_")[1]
     }
 
     @Test
     fun test_list_repos() {
-        val fileName = "list_repos"
         val responseCode = 200
 
         server.enqueue(MockResponse()
                 .setResponseCode(responseCode)
-                .setBody(APIServiceTestHelper.body(fileName, responseCode)))
+                .setBody(APIServiceTestHelper.body(methodName, responseCode)))
 
         val service = ServiceGenerator.create(GitHubService::class.java, server.url("").toString())
         val response = service.listRepos(anyString()).execute()
