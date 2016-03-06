b7158e471514029273bf15808ea3a5441da30a7f
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-06 14:57:55 +0800

    Adding failing test first

--- app/src/test/java/io/benjamintan/ankogit/activities/LoginActivityTest.kt
+++ app/src/test/java/io/benjamintan/ankogit/activities/LoginActivityTest.kt
@@ -1,17 +1,31 @@
 package io.benjamintan.ankogit.activities
 
+import android.content.Intent
+import android.util.Base64
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.natpryce.hamkrest.Matcher
 import com.natpryce.hamkrest.assertion.assertThat
+import io.benjamintan.ankogit.APIServiceTestHelper
 import io.benjamintan.ankogit.R
 import io.benjamintan.ankogit.RobolectricTest
+import io.benjamintan.ankogit.data.api.GitHubService
+import io.benjamintan.ankogit.data.api.ServiceGenerator
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.jetbrains.anko.find
 import org.junit.Test
+import org.mockito.Mockito.*
 import org.robolectric.Robolectric
+import org.robolectric.Shadows.*
+import org.robolectric.internal.ShadowExtractor
+import org.robolectric.shadows.ShadowActivity
 
 class LoginActivityTest : RobolectricTest() {
+
+    val server = MockWebServer()
+    var service = ServiceGenerator.create(GitHubService::class.java, server.url("").toString())
 
     val isEnabled = Matcher(View::isEnabled)
 
@@ -31,6 +45,26 @@ class LoginActivityTest : RobolectricTest() {
 
         password.setText("sekret")
         assertThat(signInButton, isEnabled)
+    }
+
+    @Test
+    fun sign_in_successful() {
+        server.enqueue(MockResponse()
+                .setResponseCode(200)
+                .setBody(APIServiceTestHelper.body("GET", "user", 200)))
+
+        val activity = Robolectric.setupActivity(LoginActivity::class.java)
+
+        activity.find<EditText>(R.id.login).apply { setText("username") }
+        activity.find<EditText>(R.id.password).apply { setText("password") }
+        activity.find<Button>(R.id.sign_in_btn).apply { performClick() }
+
+        val shadowActivity = ShadowExtractor.extract(activity) as ShadowActivity
+
+        val expectedIntent = Intent(activity, MainActivity::class.java)
+        val actualIntent = shadowActivity.nextStartedActivity
+
+        assert(expectedIntent.filterEquals(actualIntent))
     }
 }
 
