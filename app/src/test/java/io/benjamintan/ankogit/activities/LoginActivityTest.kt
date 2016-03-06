69b16506dfaa491dfa85265aa0c46b8fff463b1d
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-06 12:54:49 +0800

    Add more test libraries

---
+++ app/src/test/java/io/benjamintan/ankogit/activities/LoginActivityTest.kt
@@ -0,0 +1,10 @@
+package io.benjamintan.ankogit.activities
+
+import io.benjamintan.ankogit.RobolectricTest
+
+import org.junit.Assert.*
+
+/**
+ * Created by benjamintan on 3/6/16.
+ */
+class LoginActivityTest : RobolectricTest()
