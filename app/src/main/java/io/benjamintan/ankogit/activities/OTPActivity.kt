7165ca454f0c174269fcb7cfbf571908086b45a9
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-06 23:39:04 +0800

    Add RxJava adapter for Retrofit + OTP Activity

---
+++ app/src/main/java/io/benjamintan/ankogit/activities/OTPActivity.kt
@@ -0,0 +1,14 @@
+package io.benjamintan.ankogit.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.benjamintan.ankogit.R
+
+class OTPActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
+
---
+++ app/src/test/java/io/benjamintan/ankogit/activities/OTPActivityTest.kt
@@ -0,0 +1,5 @@
+package io.benjamintan.ankogit.activities
+
+/**
+ * Created by benjamintan on 3/6/16.
+ */
