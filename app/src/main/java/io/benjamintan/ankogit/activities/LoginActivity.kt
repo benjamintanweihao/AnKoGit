4870b6c16cf68be7ac15fb77199364a851dd83c7
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-06 22:06:50 +0800

    WIP

--- app/src/main/java/io/benjamintan/ankogit/activities/LoginActivity.kt
+++ app/src/main/java/io/benjamintan/ankogit/activities/LoginActivity.kt
@@ -1,22 +1,30 @@
 package io.benjamintan.ankogit.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import com.jakewharton.rxbinding.widget.RxTextView
 import io.benjamintan.ankogit.R
+import io.benjamintan.ankogit.data.api.GitHubService
+import io.benjamintan.ankogit.data.api.ServiceGenerator
 import org.jetbrains.anko.find
-import org.jetbrains.anko.toast
 import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
 import rx.lang.kotlin.subscribeWith
+import org.jetbrains.anko.*
+import android.util.Base64
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var service: GitHubService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        service = ServiceGenerator.create(GitHubService::class.java)
 
         val login = find<EditText>(R.id.login)
         val password = find<EditText>(R.id.password)
@@ -42,7 +50,13 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun doSignIn(login: String, password: String) {
-        toast("$login:$password")
+        val authStr = "$login:$password"
+        val encodedAuthStr = "Basic ${Base64.encodeToString(authStr.toByteArray(), Base64.NO_WRAP)}"
+
+        val response = service.login(encodedAuthStr).execute()
+        if (response.isSuccess) {
+            startActivity<MainActivity>()
+        }
     }
 
     private fun notBlankObservable(login: EditText): Observable<Boolean>? {
