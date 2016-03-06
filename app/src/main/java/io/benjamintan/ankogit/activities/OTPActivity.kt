0fca4308401381804b851d751cb5c24027ab872e
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-06 23:57:16 +0800

    Fleshing out OTPActivity

--- app/src/main/java/io/benjamintan/ankogit/activities/OTPActivity.kt
+++ app/src/main/java/io/benjamintan/ankogit/activities/OTPActivity.kt
@@ -2,13 +2,35 @@ package io.benjamintan.ankogit.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import io.benjamintan.ankogit.R
+import io.benjamintan.ankogit.data.api.GitHubService
+import io.benjamintan.ankogit.data.api.ServiceGenerator
+import org.jetbrains.anko.*
+import rx.lang.kotlin.subscribeWith
 
 class OTPActivity : AppCompatActivity() {
+
+    lateinit var service: GitHubService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
+
+        service = ServiceGenerator.create(GitHubService::class.java)
+
+        val otp = find<EditText>(R.id.otp)
+        val signInBtn = find<Button>(R.id.sign_in_btn)
+
+        signInBtn.setOnClickListener {
+            service
+                    .login("basic auth string", otp.text.toString())
+                    .subscribeWith {
+                        onNext { startActivity<MainActivity>() }
+                        onError { toast(it.message.toString()) }
+                    }
+        }
     }
 }
-
--- app/src/test/java/io/benjamintan/ankogit/activities/OTPActivityTest.kt
+++ app/src/test/java/io/benjamintan/ankogit/activities/OTPActivityTest.kt
@@ -1,5 +1,51 @@
 package io.benjamintan.ankogit.activities
 
-/**
- * Created by benjamintan on 3/6/16.
- */
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import io.benjamintan.ankogit.APIServiceTestHelper
+import io.benjamintan.ankogit.R
+import io.benjamintan.ankogit.RobolectricTest
+import io.benjamintan.ankogit.data.api.GitHubService
+import io.benjamintan.ankogit.data.api.ServiceGenerator
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.jetbrains.anko.find
+import org.junit.Test
+import org.robolectric.Robolectric
+import org.robolectric.internal.ShadowExtractor
+import org.robolectric.shadows.ShadowActivity
+
+class OTPActivityTest : RobolectricTest() {
+
+    val server = MockWebServer()
+
+    @Test
+    fun sign_in_button_should_enable_only_when_otp_is_filled() {
+
+    }
+
+    // TODO: Need to pass along the "Basic: auth" string over to the
+    // TODO: next activity
+    @Test
+    fun sign_in_successful() {
+        server.enqueue(MockResponse()
+                .setResponseCode(200)
+                .setBody(APIServiceTestHelper.body("GET", "user", 200)))
+
+        val activity = Robolectric.setupActivity(OTPActivity::class.java).apply {
+            service = ServiceGenerator.create(GitHubService::class.java, server.url("").toString())
+        }
+
+        val shadowActivity = ShadowExtractor.extract(activity) as ShadowActivity
+
+        activity.find<EditText>(R.id.otp).apply { setText("123456") }
+        activity.find<Button>(R.id.sign_in_btn).apply { performClick() }
+
+        val expectedIntent = Intent(activity, MainActivity::class.java)
+        val actualIntent = shadowActivity.nextStartedActivity
+        assert(expectedIntent.filterEquals(actualIntent))
+    }
+
+}
+
