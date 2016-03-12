82d75cb33602e5df57e663469e618cd4bc536a2c
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-12 08:04:23 +0800

    Bye bye, Java!

---
+++ app/src/androidTest/java/io/benjamintan/ankogit/ApplicationTest.kt
@@ -0,0 +1,6 @@
+package io.benjamintan.ankogit
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
