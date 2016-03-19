d20861b2ab8a3bb91a1d0c308deaf9a9ecce1a83
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-03-20 00:54:28 +0800

    Add event list layout + tests

--- app/src/main/java/io/benjamintan/ankogit/adapters/EventListAdapter.kt
+++ app/src/main/java/io/benjamintan/ankogit/adapters/EventListAdapter.kt
@@ -1,24 +1,47 @@
 package io.benjamintan.ankogit.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import io.benjamintan.ankogit.R
 import io.benjamintan.ankogit.data.api.Event
+import org.jetbrains.anko.find
 
 class EventListAdapter(val events: List<Event> = emptyList()) : RecyclerView.Adapter<EventListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        throw UnsupportedOperationException()
+        val v: View = LayoutInflater.from(parent.context).inflate(R.layout.event, parent, false)
+        return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        throw UnsupportedOperationException()
+        val event = events[position]
+        val userName = event.actor.login
+        val eventVerb = when(event.type) {
+            "WatchEvent" -> "starred"
+            "CreateEvent" -> "created"
+            "MemberEvent" -> "joined"
+            "ForkEvent" -> "forked"
+            else -> ""
+        }
+        val repo = event.repo.name
+
+        holder.whoAndActionTextView.text = "$userName $eventVerb"
+        holder.repoAndWhenAgoTextView.text = repo
     }
 
     override fun getItemCount(): Int = events.count()
 
-    class ViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var whoAndActionTextView: TextView
+        internal var repoAndWhenAgoTextView: TextView
 
+        init {
+            whoAndActionTextView = itemView.find<TextView>(R.id.who_and_action)
+            repoAndWhenAgoTextView = itemView.find<TextView>(R.id.repo_and_when)
+        }
     }
 }
 
--- app/src/main/java/io/benjamintan/ankogit/data/api/Repo.kt
+++ app/src/main/java/io/benjamintan/ankogit/data/api/Repo.kt
@@ -2,8 +2,8 @@ package io.benjamintan.ankogit.data.api
 
 data class Repo(
         val name: String,
-        val description: String,
+        val description: String = "",
         val stargazers_count: Int = 0,
         val watchers_count: Int = 0,
-        val language: String = "unknown",
+        val language: String = "",
         val url: String)
